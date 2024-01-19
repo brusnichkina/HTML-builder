fa7e06e2801dd53587cce01c44ea226a8671359b
brusnichkina <bru_snichkina@icloud.com>
2024-01-19 18:31:07 +0300

    feat: complete task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdout, stdin } = process;
+const pathToFile = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(pathToFile, 'utf-8');
+
+const goOutOfHere = () => {
+  stdout.write('Bye!');
+  process.exit();
+};
+
+stdout.write('Hello! Write your text here: \n');
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+rl.on('SIGINT', () => goOutOfHere());
+rl.on('line', (text) => {
+  if (text.toString().includes('exit')) {
+    goOutOfHere();
+  }
+  writeStream.write(`${text}\n`);
+});
