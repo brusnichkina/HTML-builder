204776bc472f3346f27ab4aa50e20adad7576ffa
brusnichkina <bru_snichkina@icloud.com>
2024-01-22 13:04:58 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,22 +7,35 @@ fs.mkdir(pathCopyFile, { recursive: true }, (error) => {
   if (error) {
     console.log(error);
   }
-  console.log('Folder created successfully!');
+  console.log('Folder copied successfully!');
 });
 
-fs.readdir(pathFile, (error, files) => {
+fs.readdir(pathCopyFile, (error, files) => {
   if (error) {
     console.log(error);
   }
-  files.forEach((file) => {
-    fs.copyFile(
-      path.join(pathFile, file),
-      path.join(pathCopyFile, file),
-      (error) => {
-        if (error) {
-          console.log(error);
-        }
-      },
+  for (let i = 0; i < files.length; i += 1) {
+    const pathToFile = path.join(pathCopyFile, files[i]);
+    fs.unlink(pathToFile, (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
+
+fs.readdir(pathFile, (error, files) => {
+  if (error) console.log(error);
+  else
+    files.forEach((file) =>
+      fs.copyFile(
+        path.join(pathFile, file),
+        path.join(pathCopyFile, file),
+        (error) => {
+          if (error) {
+            console.log(error);
+          }
+        },
+      ),
     );
-  });
 });
