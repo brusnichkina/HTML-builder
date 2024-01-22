eb91e4c431e41d5a2d70ab29563aab39558c9001
brusnichkina <bru_snichkina@icloud.com>
2024-01-22 09:59:08 +0300

    feat: complete task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const secretFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(secretFolder, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Names of files:');
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(secretFolder, file.name);
+        const extens = filePath.split('.').pop();
+        const nameOffFile = filePath.split('.').shift();
+        const name = nameOffFile.split('\\').pop();
+        fs.stat(filePath, (error, stat) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(
+              `${name} - ${extens} - ${(stat.size / 1024).toFixed(3)}kb`,
+            );
+          }
+        });
+      }
+    });
+  }
+});
